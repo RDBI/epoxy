e63734bec727b3183576e4cb3fa1fcc8b6450f34
Erik Hollensbe <erik@hollensbe.org>
2010-05-27 05:44:11 -0400

    * Some tests.

--- test/test_epoxy.rb
+++ test/test_epoxy.rb
@@ -1,7 +1,44 @@
 require 'helper'
 
 class TestEpoxy < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+    def test_01_basic
+        ep = Epoxy.new("select * from foo where bar=?")
+        assert(ep)
+        assert_kind_of(Epoxy, ep)
+
+        assert_equal("select * from foo where bar='foo'", ep.quote { |x| "'foo'" })
+    end
+
+    def test_02_double_question
+        ep = Epoxy.new("select ?? from foo where bar=?")
+        assert_equal("select ? from foo where bar='foo'", ep.quote { |x| "'foo'" })
+    end
+
+    def test_03_quotes_already
+        ep = Epoxy.new("select * from \"foo\" where bar=?")
+        assert_equal("select * from \"foo\" where bar='foo'", ep.quote { |x| "'foo'" })
+        
+        ep = Epoxy.new("select * from 'foo' where bar=?")
+        assert_equal("select * from 'foo' where bar='foo'", ep.quote { |x| "'foo'" })
+    end
+    
+    def test_04_holy_shit
+        ep = Epoxy.new("select * from \"'foo'\" where bar=?")
+        assert_equal("select * from \"'foo'\" where bar='foo'", ep.quote { |x| "'foo'" })
+        
+        ep = Epoxy.new("select * from E\"'foo'\" where bar=?")
+        assert_equal("select * from E\"'foo'\" where bar='foo'", ep.quote { |x| "'foo'" })
+        
+        ep = Epoxy.new("select * from E\"''foo''\" where bar=?")
+        assert_equal("select * from E\"''foo''\" where bar='foo'", ep.quote { |x| "'foo'" })
+        
+        ep = Epoxy.new("select * from E\"\\''foo''\" where bar=?")
+        assert_equal("select * from E\"\\''foo''\" where bar='foo'", ep.quote { |x| "'foo'" })
+        
+        ep = Epoxy.new("select * from E\"\\''foo\\''\" where bar=?")
+        assert_equal("select * from E\"\\''foo\\''\" where bar='foo'", ep.quote { |x| "'foo'" })
+       
+        ep = Epoxy.new("select * from foo where bar='?'")
+        assert_equal("select * from foo where bar='?'", ep.quote { |x| "'foo'" })
+    end
 end
